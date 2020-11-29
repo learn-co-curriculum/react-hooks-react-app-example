c856401984d3b03b86372404e89a1e2b6a677306
ihollander <ianholla@gmail.com>
2020-11-29 17:20:47 -0500

    Use Enzyme tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,20 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { expect } from 'chai';
+import React from 'react'
+import moment from 'moment'
+import App from '../App'
 
-test('should include "Now" in the header instead of a time', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Now/);
-  expect(headerElement).toBeInTheDocument();
-});
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
-test('should include the ExampleComponent', () => {
-  render(<App />);
-  const paragraphElement = screen.getByText(/Whoa!/);
-  expect(paragraphElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() })
 
-test('should include the TestComponent', () => {
-  render(<App />);
-  const iframe = screen.getByTitle(/time video/);
-  expect(iframe).toBeInTheDocument();  
+describe('App', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    wrapper = shallow(React.createElement(App));
+  });
+
+  it('should include "Now" in the header instead of a time', () => {
+    expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
+    expect(wrapper.find('header').text()).to.include('Now')
+  });
+
+  it('should include the ExampleComponent', () => {
+    expect(wrapper.text()).to.include('<ExampleComponent />')
+  });
+
+  it('should include the TestComponent', () => {
+    expect(wrapper.text()).to.include('<TestComponent />')
+  });
 });
