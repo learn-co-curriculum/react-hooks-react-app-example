8d0ce447d76c4a1872a402f1f484b2dbcee6a859
Liz Burton <liz.burton147@gmail.com>
2021-08-09 11:07:33 -0400

    Update starter code to match instructions

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-// import TestComponent from "./TestComponent";
 
 // Add your code own within the return statement
 function App() {
